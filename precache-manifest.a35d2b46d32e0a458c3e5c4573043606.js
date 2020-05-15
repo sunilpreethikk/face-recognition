2254eab88adefb6007ba1459f6da3c80fbbda089
Sunil Preethik <sunilpreethik@sunils-air.localdomain>
2020-05-15 16:55:49 +0200

    Updates

---
+++ precache-manifest.a35d2b46d32e0a458c3e5c4573043606.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a0198b50b39fd4fb05ad6af564ccb5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da597284c49697caf986",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a379cfb659c7e0a7d9ad",
+    "url": "/static/css/main.1280ea58.chunk.css"
+  },
+  {
+    "revision": "da597284c49697caf986",
+    "url": "/static/js/2.224b79b9.chunk.js"
+  },
+  {
+    "revision": "a379cfb659c7e0a7d9ad",
+    "url": "/static/js/main.9f047f30.chunk.js"
+  },
+  {
+    "revision": "816b29343594ddf75f35",
+    "url": "/static/js/runtime-main.322ace93.js"
+  }
+]);
